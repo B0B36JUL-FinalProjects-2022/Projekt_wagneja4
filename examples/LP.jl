11ec01da07ca0626ca2217a52893750757edb95e
wagneja4 <jenik.wagner@gmail.com>
2023-01-18 00:46:06 +0100

    added JuMP and HiGHS + example

---
+++ examples/LP.jl
@@ -0,0 +1,21 @@
+using JuMP, HiGHS
+
+m = Model();
+set_optimizer(m, HiGHS.Optimizer);
+# Define the variables
+@variable(m, x>=0);
+@variable(m, y>=0);
+
+# Define the constraints 
+@constraint(m, x+y<=5);
+@constraint(m, 10x+6y<=45);
+
+# Define the objective function
+@objective(m, Max, 5x+4y);
+
+# Run the solver
+optimize!(m);
+
+# Output
+println(objective_value(m)) # optimal value z
+println("x = ", value.(x), "\n","y = ",value.(y)) # optimal solution x & y
--- examples/examples.jl
+++ examples/examples.jl
@@ -1,2 +1,4 @@
+using Revise
 include("BinaryTree.jl")
 include("ULBoundTree.jl")
+include("LP.jl")
